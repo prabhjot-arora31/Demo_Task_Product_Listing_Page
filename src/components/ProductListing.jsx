e7f04aecfc4f9ae86901f0689c2ebff064f07a47
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-09-02 22:22:06 +0530

    Update ProductListing.jsx updating logic for more broad search

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -10,7 +10,7 @@ const ProductListing = () => {
       setProducts(
         allProducts.filter((product) => {
           return (
-            product.name.toLowerCase() === searchedTerm.toLowerCase().trim()
+            product.name.toLowerCase().includes(searchedTerm.toLowerCase().trim())
           );
         })
       );
